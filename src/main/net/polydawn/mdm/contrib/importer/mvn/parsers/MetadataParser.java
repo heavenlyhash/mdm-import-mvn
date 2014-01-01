3a1a35dc2e464462f8fc7013e11258fa68cb0409
Eric Myhre <hash@exultant.us>
2014-01-01 00:45:05 -0600

    things i am not doing now includes

--- src/main/net/polydawn/mdm/contrib/importer/mvn/parsers/MetadataParser.java
+++ src/main/net/polydawn/mdm/contrib/importer/mvn/parsers/MetadataParser.java
@@ -34,6 +34,11 @@ public class MetadataParser {
 			    <lastUpdated>20121207031623</lastUpdated>
 			  </versioning>
 			</metadata>
+		 *
+		 * We could also get maven-metadata.xml.md5 or maven-metadata.xml.sha1, but honestly,
+		 * I'm fine assuming the byte stream wasn't corrupted if the xml parsed, the tcp crc passed,
+		 * and so forth.  And it's not like there's a security property gained by verifying a
+		 * checksum that I just downloaded over http.
 		 */
 
 		NodeList nl = xml.getElementsByTagName("version");

fbaa913f9ca4a2660ee81b8d53fd7bcd9358a907
Eric Myhre <hash@exultant.us>
2014-01-01 01:53:29 -0600

    MAAAAAAAVVVVVVEENNNNNNNNNNN html parsing died for your sins

--- src/main/net/polydawn/mdm/contrib/importer/mvn/parsers/PomParser.java
+++ src/main/net/polydawn/mdm/contrib/importer/mvn/parsers/PomParser.java
@@ -75,6 +75,23 @@ public class PomParser {
 		 * But how the fuck are you supposed to find that out from this file?  You can't.
 		 * And nowhere in this pom, nor the parent metadata xml, was a project type of "java" specified, even.
 		 * You basically have to read maven's mind, here, apparently.
+		 *
+		 * Oh.  Okay, so... apparently, "jar" is the default... classifier?  Type?  Packaging?  The docs can't keep it straight and neither can I.
+		 * And the rest of the "-sources" and "-javadoc" classifiers (pretty sure those are definitely classifiers)...
+		 * they don't mean anything.  Unless someone asks for them.  Huh.
+		 * And as far as I can tell, there really *isn't* an API in maven to ask for those secondary artifacts.
+		 * Other plugins that talk about sources or whatever just ask for them because that's what *that plugin* expects the file to be called.
+		 *
+		 * Related experiment:
+		 * `mvn org.apache.maven.plugins:maven-dependency-plugin:2.1:get -DrepoUrl=url -Dartifact=org.springframework.android:spring-android-core:1.0.1.RELEASE`
+		 * (and yes, -DrepoUrl=bullshit is *necessary*... if you don't provide it, it errors, but if you do, then maven calls your bullshit and drops in a reasonable default, yes I'm serious)
+		 * just gets these two files:
+		 * - spring-android-core-1.0.1.RELEASE.jar
+		 * - spring-android-core-1.0.1.RELEASE.jar.sha1
+		 * none of the other classifiers, not even the .md5 for the jar, or the .asc with the sig.
+		 *
+		 * So, I guess we're going to curl the html listing made for humans,
+		 * and extract the godforsaken links to actual files from that.
 		 */
 
 		// fuck

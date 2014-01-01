d568a4696389c3125dc9ae347426bc0408dd814d
Eric Myhre <hash@exultant.us>
2014-01-01 03:19:50 -0600

    when life gives you shit xml, you give life Knuth-Morris-Pratt and a backhand of whitespace.  tests now pass.

--- src/main/net/polydawn/mdm/contrib/importer/mvn/util/Xml.java
+++ src/main/net/polydawn/mdm/contrib/importer/mvn/util/Xml.java
@@ -1,12 +1,24 @@
 package net.polydawn.mdm.contrib.importer.mvn.util;
 
 import java.io.*;
+import java.util.*;
 import javax.xml.parsers.*;
 import org.w3c.dom.*;
 import org.xml.sax.*;
 
 public class Xml {
 	public static Element parse(byte[] bats) throws IOException {
+
+		// replace <hr> tags with blank spaces
+		// fuck you, that's why
+		List<Integer> bads = allIndexOf(bats, new byte[] { '<', 'h', 'r', '>' });
+		for (int bad : bads) {
+			bats[bad++] = ' ';
+			bats[bad++] = ' ';
+			bats[bad++] = ' ';
+			bats[bad++] = ' ';
+		}
+
 		try {
 			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 			dbf.setValidating(false);
@@ -15,7 +27,46 @@ public class Xml {
 		} catch (ParserConfigurationException e) {
 			throw new Error("you what?", e);
 		} catch (SAXException e) {
-			throw new IOException ("shit xml.", e);
+			throw new IOException("shit xml.", e);
 		}
+	}
+
+	// get some Knuth-Morris-Pratt all up in here
+	public static List<Integer> allIndexOf(byte[] data, byte[] pattern) {
+		int[] failure = computeKmpFailure(pattern);
+
+		int j = 0;
+		if (data.length == 0) return Collections.EMPTY_LIST;
+		if (pattern.length > data.length) return Collections.EMPTY_LIST;
+
+		List<Integer> answer = new LinkedList<Integer>();
+		for (int i = 0; i < data.length; i++) {
+			while (j > 0 && pattern[j] != data[i]) {
+				j = failure[j - 1];
+			}
+			if (pattern[j] == data[i]) {
+				j++;
+			}
+			if (j == pattern.length) {
+				answer.add(i - pattern.length + 1);
+				j = 0;
+			}
+		}
+		return answer;
+	}
+
+	private static int[] computeKmpFailure(byte[] pattern) {
+		int[] failure = new int[pattern.length];
+
+		int j = 0;
+		for (int i = 1; i < pattern.length; i++) {
+			while (j > 0 && pattern[j] != pattern[i])
+				j = failure[j - 1];
+			if (pattern[j] == pattern[i])
+				j++;
+			failure[i] = j;
+		}
+
+		return failure;
 	}
 }
